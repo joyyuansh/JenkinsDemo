803c88d4ed441f6b906a4f209f8e578b2183275c
Joy Yuan <Joy.Yuan@vector.com>
2018-05-04 16:33:56 +0800

    test

--- calculator.c
+++ calculator.c
@@ -28,13 +28,11 @@ int divd(int a, int b)
         return a/b ;
 }
 
-/*
-int divdf(float a, float b)
+float divdf(float a, float b)
 {
     if(b!=0)
         return a/b;	
 }
-*/
 
 point add_point(point a, point b){
     point val;
